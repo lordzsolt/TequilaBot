a74cbbc9c995bfc11d9667485e25036475d1470b
Zsolt Kovacs <lordzsoltt@gmail.com>
2023-03-06 23:13:28 +0100

    Do not copy config.json for now, to aid with testing

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ DESTINATION=lord@iosmith.com
 GOOS=linux GOARCH=amd64 go build -o bin/app-amd64-linux .
 
 ssh $DESTINATION 'sudo systemctl stop saltbot.service'
-scp config.json $DESTINATION:/home/saltbot/config.json
+# scp config.json $DESTINATION:/home/saltbot/config.json
 scp bin/app-amd64-linux $DESTINATION:/home/saltbot/saltbot.bin
 ssh $DESTINATION 'sudo systemctl start saltbot.service'
